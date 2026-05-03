b6cf2d940032c82893f28e10bce37823c12e664d
Samara Soares <samarafernandessoares97@gmail.com>
2026-05-03 17:50:14 -0300

    Refactor Playlist class methods and error messages

--- Streaming/Playlist.java
+++ Streaming/Playlist.java
@@ -2,51 +2,45 @@ import java.util.ArrayList;
 
 public class Playlist {
 
-    String nome;
-    ArrayList<Musica> musicas = new ArrayList<>();
+    String nome; 
+    ArrayList<Musica> musicas = new ArrayList<>(); 
 
-    
-    void adicionarMusica(Musica musica)
-{
-     this.musicas.add(musica);
-     System.out.println("Música " + musica.getTitulo() + " adicionada á playlist!");
-   }
- 
-    void removerMusica(int indice){
-
-     if ( indice >= 0 && indice < this.musicas.size()) {
-
-        this.musicas.remove(indice);
-        System.out.println("Erro: Índice da musica inválido!");
-     }
+    void adicionarMusica(Musica musica) {
+        this.musicas.add(musica);
+        System.out.println("Música " + musica.getTitulo() + " adicionada á playlist!");
     }
 
-    void listarMusicas(){
+    void removerMusica(int indice) {
+        if (indice >= 0 && indice < this.musicas.size()) {
+            this.musicas.remove(indice);
+
+            System.out.println("Música removida com sucesso!"); 
+        } else {
+            System.out.println("Erro: Índice da musica inválido!");
+        }
+    }
+
+    void listarMusicas() {
         System.out.println("\n=== Playlist: " + this.nome + "===");
-     if (this.musicas.isEmpty()) {
+        if (this.musicas.isEmpty()) {
             System.out.println("Está playlist ainda não tem músicas.");
-            
-        } else{
-            for (int i = 0; i < this.musicas.size(); i++){
-                System.out.println(i + ".");
-
-            this.musicas.get(i).exibir();
-
+        } else {
+            for (int i = 0; i < this.musicas.size(); i++) {
+                System.out.print(i + ". ");
+                this.musicas.get(i).exibir();
             }
         }
     }
 
-       int getDuracaoTotal(){
-           int total = 0;
+    int getDuracaoTotal() {
+        int total = 0;
+        for (Musica m : this.musicas) {
+            total += m.getDuracaoSegundos();
+        }
+        return total;
+    }
 
-           for (Musica m : this.musicas){
-                total += m.getDuracaoSegundos();
-           }
-           return total;
-       }
-
-       int getDuracaoMusicas(){
-           return this.musicas.size();
-       }
-
+    int getDuracaoMusicas() {
+        return this.musicas.size();
+    }
 }
