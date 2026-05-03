0410cfa8217adcfed3007c3f319d6038018dde39
Samara Soares <samarafernandessoares97@gmail.com>
2026-05-03 17:58:49 -0300

    Add PlaylistPersonalizada class with reproduction method

--- Streaming/PlaylistPersonalizada.java
+++ Streaming/PlaylistPersonalizada.java
@@ -1,1 +1,12 @@
+public class PlaylistPersonalizada extends Playlist {
+    
+    public PlaylistPersonalizada(String nome) {
+        super(nome);
+    }
 
+    @Override
+    public void reproduzir() {
+        System.out.println(" [Playlist Personalizada] " + this.nome);
+        super.reproduzir();
+    }
+}
