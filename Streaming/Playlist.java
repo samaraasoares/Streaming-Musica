a3617dc9e4e778dd5f8a54434f258375a61e7b02
Samara Soares <samarafernandessoares97@gmail.com>
2026-05-04 18:43:57 -0300

    Add description field to Playlist class
    Updated the Playlist class to include a description field and modified methods accordingly.

--- Streaming/Playlist.java
+++ Streaming/Playlist.java
@@ -1,62 +1,31 @@
 import java.util.ArrayList;
 
 public class Playlist {
-
-   
     protected String nome;
+    protected String descricao; 
     protected ArrayList<Musica> musicas = new ArrayList<>();
 
-    public Playlist(String nome) {
+    public Playlist(String nome, String descricao) {
         this.nome = nome;
+        this.descricao = descricao;
     }
     
     public void adicionarMusica(Musica musica) {
         this.musicas.add(musica);
-        System.out.println("Música " + musica.getTitulo() + " adicionada à playlist!");
     }
  
-    public void removerMusica(int indice) {
-
-        if (indice >= 0 && indice < this.musicas.size()) {
-            this.musicas.remove(indice);
-            System.out.println("Música removida com sucesso!");
-        } else {
-            System.out.println("Erro: Índice da música inválido!");
-        }
-    }
-
     public void reproduzir() {
         System.out.println("\n Reproduzindo playlist: " + this.nome);
+        System.out.println(" Descrição: " + this.descricao);
         if (this.musicas.isEmpty()) {
-            System.out.println("A playlist está vazia.");
+            System.out.println("   A playlist está vazia.");
         } else {
             for (Musica m : musicas) {
-                System.out.println("   " + m.getTitulo() + " - " + m.getArtista());
+                System.out.println("    " + m.getTitulo() + " - " + m.getArtista());
             }
         }
     }
 
-    public void listarMusicas() {
-        System.out.println("\n=== Playlist: " + this.nome + " ===");
-        if (this.musicas.isEmpty()) {
-            System.out.println("Esta playlist ainda não tem músicas.");
-        } else {
-            for (int i = 0; i < this.musicas.size(); i++) {
-                System.out.print(i + ". ");
-                this.musicas.get(i).exibir();
-            }
-        }
-    }
-
-    public int getDuracaoTotal() {
-        int total = 0;
-        for (Musica m : this.musicas) {
-            total += m.getDuracaoSegundos();
-        }
-        return total;
-    }
-
-    public int getQuantidadeMusicas() {
-        return this.musicas.size();
-    }
+    public String getNome() { return nome; }
+    public String getDescricao() { return descricao; }
 }
