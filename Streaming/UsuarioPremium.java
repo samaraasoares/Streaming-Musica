3d4a3d6315349f208e4c326587788ae2b032767e
Samara Soares <samarafernandessoares97@gmail.com>
2026-05-04 18:46:30 -0300

    Refactor UsuarioPremium class and enhance methods
    Removed unused methods and improved music download feedback.

--- Streaming/UsuarioPremium.java
+++ Streaming/UsuarioPremium.java
@@ -13,29 +13,15 @@ public class UsuarioPremium extends Usuario {
 
     @Override
     public void reproduzirMusica(Musica musica) {
-
-        System.out.println("💎 [ALTA QUALIDADE] Reproduzindo: " + musica.getTitulo());
+        System.out.println(" [ALTA QUALIDADE] Reproduzindo: " + musica.getTitulo());
         historicoReproducao.add(musica);
+        totalReproducoesSistema++;
     }
 
     public void baixarMusica(Musica musica) {
-        if (musica != null) {
-            musicasBaixadas.add(musica);
-            System.out.println("⬇️ Música '" + musica.getTitulo() + "' baixada com sucesso!");
-        }
-    }
-
-    public void listarMusicasBaixadas() {
-        System.out.println("\n--- MÚSICAS BAIXADAS (OFFLINE) ---");
-        if (musicasBaixadas.isEmpty()) {
-            System.out.println("Nenhuma música baixada.");
-        } else {
-            for (Musica m : musicasBaixadas) {
-                System.out.println("- " + m.getTitulo());
-            }
-        }
+        musicasBaixadas.add(musica);
+        System.out.println(" Música '" + musica.getTitulo() + "' baixada!");
     }
 
     public String getTipoPlano() { return tipoPlano; }
-    public void setTipoPlano(String tipoPlano) { this.tipoPlano = tipoPlano; }
 }
