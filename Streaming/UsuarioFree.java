8a4e1388a95d1ee4552212d0c9dc6ef9d9401242
Samara Soares <samarafernandessoares97@gmail.com>
2026-05-04 18:46:02 -0300

    Refactor UsuarioFree to track total ads displayed

--- Streaming/UsuarioFree.java
+++ Streaming/UsuarioFree.java
@@ -1,31 +1,31 @@
 public class UsuarioFree extends Usuario {
-    private int contadorReproducoes;
+    private int contadorReproducoesLocal;
+    public static int totalAnunciosExibidos = 0; 
     private static final int MAX_PLAYLISTS = 3;
 
     public UsuarioFree(String nome, String email) {
         super(nome, email);
-        this.contadorReproducoes = 0;
+        this.contadorReproducoesLocal = 0;
     }
 
     @Override
     public void reproduzirMusica(Musica musica) {
-        contadorReproducoes++;
-        
-        if (contadorReproducoes % 3 == 0) {
+        contadorReproducoesLocal++;
+        if (contadorReproducoesLocal % 3 == 0) {
             exibirAnuncio(); 
         }
-        
         super.reproduzirMusica(musica); 
     }
 
     private void exibirAnuncio() {
-        System.out.println("\n[ANÚNCIO] Assine o Premium para ouvir sem interrupções! \n");
+        System.out.println("\n [ANÚNCIO] Assine o Premium para ouvir sem interrupções!");
+        totalAnunciosExibidos++;
     }
 
     @Override
     public void adicionarPlaylist(Playlist playlist) {
         if (playlists.size() >= MAX_PLAYLISTS) {
-            System.out.println(" Limite de playlists atingido!");
+            System.out.println(" Limite de 3 playlists atingido para conta Free!");
         } else {
             super.adicionarPlaylist(playlist);
         }
