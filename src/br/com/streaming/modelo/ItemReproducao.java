c0a4d55bfb8d93ed826163f635424ee1a18624d7
Samara Soares <samarafernandessoares97@gmail.com>
2026-05-15 11:17:36 -0300

    Add abstract ItemReproducao class with properties

--- src/br/com/streaming/modelo/ItemReproducao.java
+++ src/br/com/streaming/modelo/ItemReproducao.java
@@ -1,1 +1,16 @@
 
+package br.com.streaming.modelo;
+
+import br.com.streaming.servico.Reproduzivel;
+
+public abstract class ItemReproducao implements Reproduzivel {
+    protected String titulo;
+    protected int duracaoSegundos;
+
+    public ItemReproducao(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getTitulo() { return titulo; }
+    @Override public int getDuracaoTotal() { return duracaoSegundos; }
+}
